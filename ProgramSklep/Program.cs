1c1ba84730c3a25bb3f649100fe8bb1694d2acd0
KSchlagowski <mediewilnp@gmail.com>
2019-12-21 18:12:30 +0100

    Pierwsze pliki

---
+++ ProgramSklep/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProgramSklep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x="";
+            //Menu
+            Console.WriteLine("Witaj w sklepie"+x);
+            x = System.Convert.ToString(Console.ReadKey());
+            System.Console.WriteLine(x);
+        }
+    }
+}
